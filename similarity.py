2678423c455b1fbe009db26b53b9d40439657634
Francis Tseng <f+accounts@frnsys.com>
2018-10-03 16:31:17 +0800

    document similarity sketch

---
+++ similarity.py
@@ -0,0 +1,30 @@
+import math
+import numpy as np
+
+# Using RV coefficient, as described in the WISDM paper
+
+def RV(X, Y):
+    # <https://www.utdallas.edu/~herve/Abdi-RV2007-pretty.pdf>
+    S_11 = np.matmul(X,X.T)
+    S_22 = np.matmul(Y,Y.T)
+    S_12 = np.matmul(X,Y.T)
+
+    # S_21 = np.matmul(Y,X.T)
+    # n = np.trace(np.dot(S_12, S_21))
+    # d = np.trace(S_11**2) * np.trace(S_22**2)
+
+    n = np.trace(S_12)
+    d = np.trace(S_11) * np.trace(S_22)
+    RV = n/math.sqrt(d)
+
+    # Convert to dissimilarity
+    # <https://www.researchgate.net/profile/Francois_Husson/publication/255569780_Testing_the_signiflcance_of_the_RV_coefficient/links/5548e4250cf205bce7abfba0/Testing-the-signiflcance-of-the-RV-coefficient.pdf>
+    dis = math.sqrt(2) * math.sqrt(1 - (RV))
+    return 1 - dis
+
+
+if __name__ == '__main__':
+    a = np.random.random((5,8)).T
+    b = np.random.random((5,12)).T
+    print(RV(a,b))
+    print(RV(a,a))
