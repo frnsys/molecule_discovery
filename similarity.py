26f809a31b33bb7e3891d4a67fa2bc1654c2962c
Francis Tseng <f+accounts@frnsys.com>
2018-10-03 16:59:52 +0800

    similarity cleanup

--- similarity.py
+++ similarity.py
@@ -1,8 +1,9 @@
 import math
 import numpy as np
 
-# Using RV coefficient, as described in the WISDM paper
+SQRT2 = math.sqrt(2)
 
+# Using RV coefficient, as described in the WISDM paper
 def RV(X, Y):
     # <https://www.utdallas.edu/~herve/Abdi-RV2007-pretty.pdf>
     S_11 = np.matmul(X,X.T)
@@ -15,16 +16,19 @@ def RV(X, Y):
 
     n = np.trace(S_12)
     d = np.trace(S_11) * np.trace(S_22)
-    RV = n/math.sqrt(d)
+    return n/math.sqrt(d)
 
+
+def sim(X, Y):
+    rv = RV(X,Y)
     # Convert to dissimilarity
     # <https://www.researchgate.net/profile/Francois_Husson/publication/255569780_Testing_the_signiflcance_of_the_RV_coefficient/links/5548e4250cf205bce7abfba0/Testing-the-signiflcance-of-the-RV-coefficient.pdf>
-    dis = math.sqrt(2) * math.sqrt(1 - (RV))
+    dis = SQRT2 * math.sqrt(1 - rv)
     return 1 - dis
 
 
 if __name__ == '__main__':
     a = np.random.random((5,8)).T
     b = np.random.random((5,12)).T
-    print(RV(a,b))
-    print(RV(a,a))
+    print(sim(a,b))
+    print(sim(a,a))
