3a8f5293f2cf8a4d8932edaf9fe96191c3045fba
Francis Tseng <f+accounts@frnsys.com>
2018-10-06 12:56:58 +0800

    tqdm tweak

--- data.py
+++ data.py
@@ -5,14 +5,14 @@ from collections import defaultdict
 
 def stream_file(path, limit=None):
     with open(path, 'r') as f:
-        for i, line in tqdm(enumerate(f)):
+        for i, line in enumerate(f):
             if limit is not None and i >= limit:
                 break
             yield line
 
 
 def stream_tokens(limit=None):
-    for line in stream_file('data/pubmed.dat', limit):
+    for line in tqdm(stream_file('data/pubmed.dat', limit)):
         doc = json.loads(line)
         pmid = doc['pmid']
         toks = doc['toks']
@@ -24,11 +24,11 @@ def load_cid2doc(articles=True, patents=False, limit=None):
     """Group documents mentioning compounds"""
     cids = defaultdict(list)
     if articles:
-        for line in stream_file('data/CID-PMID', limit):
+        for line in tqdm(stream_file('data/CID-PMID', limit)):
             cid, pmid, _ = line.strip().split()
             cids[int(cid)].append(int(pmid))
     if patents:
-        for line in stream_file('data/CID-Patent', limit):
+        for line in tqdm(stream_file('data/CID-Patent', limit)):
             cid, ptid = line.strip().split('\t')
             cids[int(cid)].append(ptid)
     return cids
