c81ced682f46db233a5c68e759fd9de452b41567
Francis Tseng <f+accounts@frnsys.com>
2018-10-08 15:10:20 +0800

    switching to generating smiles for all SDF files

--- data/readme.md
+++ data/readme.md
@@ -2,6 +2,7 @@ At time of writing:
 
 - the PubChem compounds database has 134,825,000 compounds.
 - 1,832,286 compounds have associated PubMed articles.
+- 22,626,944 compounds across the PubMed and patent CID listings.
 - there are 8,659,712 PubMed articles associated with PubChem compounds.
 - there are 2,150,543 Open Access PubMed articles.
 - there are 493,828 Open Access PubMed articles associated with PubChem compounds.
@@ -13,5 +14,5 @@ To download PubChem compound SDF data (_warning: requires ~600GB of space_) and 
 
 To generate derived data (_warning: this will take a several hours, days even_):
 
-1. `python to_smiles.py` to convert SDF data for compounds with associated PubMed articles into SMILES format. Results in a `smiles` folder.
+1. `python to_smiles.py` to convert SDF data for compounds into SMILES format. Results in a `smiles` folder.
 2. `python to_docs.py` to filter PubMed articles to those with PubChem compounds and extract and tokenize their titles and abstracts. Results in a `pubmed.dat` file.
--- data/to_smiles.py
+++ data/to_smiles.py
@@ -1,50 +1,40 @@
 import os
 from tqdm import tqdm
 from glob import glob
-from rdkit import Chem
+from rdkit import Chem, RDLogger
 from multiprocessing import Pool
 
-# Get PubMed CIDs
-cids = set()
-with open('CID-PMID', 'r') as f:
-    for line in tqdm(f):
-        line = line.strip()
-        cid, pmid, _ = line.split()
-        cids.add(int(cid))
+# Silence logs
+lg = RDLogger.logger()
+lg.setLevel(RDLogger.CRITICAL)
 
 
 def to_smiles(sdf):
     path, _ = os.path.splitext(sdf)
     fname = os.path.basename(path)
-
-    # Skip SDF files that don't include a PubMed compound
-    _, range_l, range_u = fname.split('_')
-    range_l, range_u = int(range_l), int(range_u)
-    if not any(cid >= range_l and cid <= range_u for cid in cids):
-        return
-
     output = os.path.join('smiles', '{}.smi'.format(fname))
     if os.path.exists(output):
         return
     smiles = []
-    suppl = Chem.SDMolSupplier(sdf)
-    for mol in suppl:
-        if mol is None: continue
-        mol_id = int(mol.GetProp('PUBCHEM_COMPOUND_CID'))
-        if mol_id not in cids:
-            continue
-        smile = Chem.MolToSmiles(mol, isomericSmiles=True)
-        smiles.append('{}\t{}'.format(mol_id, smile))
-    with open(output, 'w') as f:
-        f.write('\n'.join(smiles))
+    try:
+        suppl = Chem.SDMolSupplier(sdf)
+        for mol in suppl:
+            if mol is None: continue
+            mol_id = int(mol.GetProp('PUBCHEM_COMPOUND_CID'))
+            smile = Chem.MolToSmiles(mol, isomericSmiles=True)
+            smiles.append('{}\t{}'.format(mol_id, smile))
+        with open(output, 'w') as f:
+            f.write('\n'.join(smiles))
+    except RuntimeError:
+        # Some SDF files couldn't be parsed properly
+        pass
 
 
 if __name__ == '__main__':
-    print(len(cids), 'PubMed compounds')
-
     if not os.path.exists('smiles'):
         os.makedirs('smiles')
 
     p = Pool()
-    for _ in tqdm(p.imap(to_smiles, glob('sdf/*.sdf'))): pass
+    files = glob('sdf/*.sdf')
+    for _ in tqdm(p.imap(to_smiles, files), total=len(files)): pass
     p.close()
